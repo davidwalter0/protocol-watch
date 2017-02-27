085f543bdd13087341a6eab91114cf1eada92c88
David Walter <walter.david@gmail.com>
2017-02-27 01:40:20 -0500

    working with some expanded functionality
    ui element for port/hosts
    ui element to name subscription topic
    ui formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,45 @@
 import { Component } from '@angular/core';
 import { MqttService, MqttConnectionState } from 'angular2-mqtt';
-
 import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="container" *ngIf="(mqtt.state | async) === MqttConnectionState.CONNECTED">
-      <div class="row">
-        <div class="col-sm-6">
-          <subscribe></subscribe>
-        </div>
-        <div class="col-sm-6">
-          <publish></publish>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <h1 *ngIf="(mqtt.state | async) === MqttConnectionState.CONNECTING">Connecting to MQTT Broker</h1>
-      <h1 *ngIf="(mqtt.state | async) === MqttConnectionState.CLOSED">MQTT Connection Closed</h1>
-    </div>
-  `
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
+  public static port = 9001;;
+  public static host = 'localhost';
+  public port: number = AppComponent.port;
+  public host: string = AppComponent.host;
+
   public MqttConnectionState = MqttConnectionState;
   public connectionState: Observable<MqttConnectionState>;
   public jsonObject;
 
   constructor(private mqtt: MqttService) {
+  }
 
+  onSubmit(formData) {
+    if (formData.valid) {
+      console.log(JSON.stringify(formData));
+      this.updateConnection(formData.host, formData.port);
+    }
+  }
+
+  updateConnection(host: string, port: number) {
+    AppComponent.host = host;
+    AppComponent.port = port;
+    this.host = host;
+    this.port = port;
+    delete this.mqtt;
+    this.mqtt = mqttServiceFactory();
   }
 }
+
+export function mqttServiceFactory() {
+  return new MqttService({
+    hostname: AppComponent.host,
+    port: AppComponent.port,
+    path: '/'
+  });
+};
