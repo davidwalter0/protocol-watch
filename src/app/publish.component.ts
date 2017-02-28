8b04d6f5697de8b865eff0346e680121d2071e30
David Walter <walter.david@gmail.com>
2017-02-28 18:03:13 -0500

    fix topic subscription from watch

--- src/app/publish.component.ts
+++ src/app/publish.component.ts
@@ -12,8 +12,11 @@ import * as root from '../protobuf/value.protobuf.js';
 export class PublishComponent {
   public error: any;
   public MqttConnectionState = MqttConnectionState;
-
+  public topic: string;
   ngOnInit() {
+    this.watchMqttService.topicStream.subscribe((topic) => {
+      this.topic = topic;
+    });
   }
 
   constructor(public watchMqttService: WatchMqttService) {
@@ -21,7 +24,7 @@ export class PublishComponent {
 
   public publish($message: string, $text: string) {
     const buffer = root.example.Value.encode({ value: $message, text: $text }).finish();
-    this.watchMqttService.mqtt.unsafePublish(SubscribeComponent.topic, buffer);
+    this.watchMqttService.mqtt.unsafePublish(this.topic, buffer);
   }
 
   onSubmit(formData) {
